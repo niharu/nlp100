edc9d84e19745295b707a3afb041e934721dbba7
niharu <wv.nsmr@gmail.com>
2018-01-31 22:17:04 +0900

    Update to One-Liner

--- chapter01/nlp02.py
+++ chapter01/nlp02.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
-    result = "".join([s1+s2 for (s1,s2) in zip("パトカー","タクシー")])
-    print(result)
+    print("".join([s1+s2 for (s1,s2) in zip("パトカー","タクシー")]))
