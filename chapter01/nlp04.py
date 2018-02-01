ff3ace66f40e5a7a1a9880d799a1d23bd2423ab9
niharu <wv.nsmr@gmail.com>
2018-02-01 21:56:38 +0900

    Update to dict

--- chapter01/nlp04.py
+++ chapter01/nlp04.py
@@ -3,4 +3,6 @@
 if __name__ == "__main__":
     ele_str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
     single_list = [1, 5, 6, 7, 8, 9, 15, 16, 19]
-    print([s[0] if i+1 in single_list else s[:2] for i,s in enumerate(ele_str.split())]) 
+    dict = {i+1:s[0] if i+1 in single_list else s[:2] for i,s in enumerate(ele_str.split())}
+    print(dict)
+
