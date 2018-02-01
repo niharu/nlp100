72b8283f5ce75cd13cb9be40945952bf99bf9d76
niharu <wv.nsmr@gmail.com>
2018-02-02 08:42:10 +0900

    Update to function

--- chapter01/nlp05.py
+++ chapter01/nlp05.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
+def n_gram(nlist, n):
+    n_gram_list = []
+    n_gram_list.append(nlist[0:len(nlist)-(n-1)])
+    n_gram_list.extend([nlist[i:] for i in range(1,n)])
+    return zip(*n_gram_list)
+
+def n_gram_word(sentence, n):
+    return n_gram(sentence.split(), n)
+
+def n_gram_char(sentence, n):
+    return n_gram(list(sentence), n)
+
 if __name__ == "__main__":
     s = "I am an NLPer"
-    word_list = s.split()
-    word_bi_gram = zip(word_list[0:len(word_list)-1],word_list[1:])
-    print(*word_bi_gram)
+    print(*n_gram_word(s, 2))
+    print(*n_gram_char(s, 2))
 
-    char_list = list(s)
-    char_bi_gram = [c1+c2 for c1,c2 in zip(char_list[0:len(char_list)-1],char_list[1:])]
-    print(char_bi_gram)
