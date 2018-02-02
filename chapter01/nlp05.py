c14d4f996f86011262677f65c5baa6c6fd4e26cd
niharu <wv.nsmr@gmail.com>
2018-02-03 08:54:51 +0900

    Update to simple

--- chapter01/nlp05.py
+++ chapter01/nlp05.py
@@ -6,14 +6,8 @@ def n_gram(nlist, n):
     n_gram_list.extend([nlist[i:] for i in range(1,n)])
     return zip(*n_gram_list)
 
-def n_gram_word(sentence, n):
-    return n_gram(sentence.split(), n)
-
-def n_gram_char(sentence, n):
-    return n_gram(list(sentence), n)
-
 if __name__ == "__main__":
     s = "I am an NLPer"
-    print(*n_gram_word(s, 2))
-    print(*n_gram_char(s, 2))
+    print(*n_gram(s.split(), 2)) # 単語bi-gram
+    print(*n_gram(list(s), 2)) # 文字bi-gram
 
