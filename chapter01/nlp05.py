38bea70412bae130944f8c5ff34e38f613815652
niharu <wv.nsmr@gmail.com>
2018-02-03 09:05:06 +0900

    Change zip to list

--- chapter01/nlp05.py
+++ chapter01/nlp05.py
@@ -4,10 +4,10 @@ def n_gram(nlist, n):
     n_gram_list = []
     n_gram_list.append(nlist[0:len(nlist)-(n-1)])
     n_gram_list.extend([nlist[i:] for i in range(1,n)])
-    return zip(*n_gram_list)
+    return list(zip(*n_gram_list))
 
 if __name__ == "__main__":
     s = "I am an NLPer"
-    print(*n_gram(s.split(), 2)) # 単語bi-gram
-    print(*n_gram(list(s), 2)) # 文字bi-gram
+    print("単語bi-gram",n_gram(s.split(), 2))
+    print("文字bi-gram",n_gram(list(s), 2))
 
