23f128398785742b530db361e61d8d4c3744b1e3
niharu <wv.nsmr@gmail.com>
2018-02-03 10:44:04 +0900

    Update

--- chapter01/nlp05.py
+++ chapter01/nlp05.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
-def n_gram(nlist, n):
-    return [nlist[i:i+n] for i in range(0, len(nlist) - (n -1))]
+def _n_gram(target, n):
+    return [target[i:i+n] for i in range(0, len(target) - (n -1))]
+
+def word_n_gram(target, n):
+    return [" ".join(s) for s in _n_gram(target.split(), n)]
+
+def char_n_gram(target, n):
+    return ["".join(s) for s in _n_gram(list(target), n)]
 
 if __name__ == "__main__":
     s = "I am an NLPer"
-    print("単語bi-gram",n_gram(s.split(), 2))
-    print("文字bi-gram",n_gram(list(s), 2))
+    print("単語bi-gram",word_n_gram(s, 2))
+    print("文字bi-gram",char_n_gram(s, 2))
+
--- chapter01/nlp06.py
+++ chapter01/nlp06.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from nlp05 import char_n_gram
+
 if __name__ == "__main__":
     s1 = "paraparaparadise"
     s2 = "paragraph"
-    clist1 = list(s1)
-    clist2 = list(s2)
-    setX = set([ c1+c2 for c1,c2 in zip(clist1[0:len(clist1)-1], clist1[1:])])
-    setY = set([ c1+c2 for c1,c2 in zip(clist2[0:len(clist2)-1], clist2[1:])])
-    print("X", sorted(setX))
-    print("Y", sorted(setY))
+    setX = set(char_n_gram(list(s1),2))
+    setY = set(char_n_gram(list(s2),2))
+    print("X", setX)
+    print("Y", setY)
     print("和集合", setX | setY)
     print("積集合", setX & setY)
     print("差集合(X-Y)", setX - setY)
